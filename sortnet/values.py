3fefdb15e446e1022f156895e2711ec7680df81c
ATUGO <tao.gu22@gmail.com>
2026-06-04 22:49:01 +0100

    move greedy algorithm to candidates.py

--- sortnet/values.py
+++ sortnet/values.py
@@ -15,20 +15,3 @@ def v_fwd(states: np.ndarray) -> dict:
             v[(i, j)] = (j - i) * count
     
     return v
-
-def greedy(v: dict, n:int) -> list:
-    """
-    Greedy algorithm: repeatedly pick the highest-value pair
-    """
-    # sort v by descending order on the number of inversions 
-    pairs = sorted(v.items(), key=lambda x: -x[1])
-
-    used = set() # track the channels alraedy used
-    layer = [] # initialize the layer
-
-    for (i, j), val in pairs:
-        if val > 0 and i not in used and j not in used: 
-            layer.append((i, j))
-            used.add(i)
-            used.add(j)
-    return layer
