9fb427c533019a40e46a28692adbd776ec3857db
ATUGO <tao.gu22@gmail.com>
2026-06-04 23:24:36 +0100

    restructure

--- experiments.py
+++ experiments.py
@@ -1,3 +1,6 @@
+# import sys, os
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
 import time
 from primitives import all_binary_inputs, apply_layer, total_inversions
 from candidates import greedy_build
